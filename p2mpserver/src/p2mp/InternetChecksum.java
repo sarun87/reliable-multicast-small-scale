855b2e3e7e39ac2dcff321cbe66c21d592c6d73b
sarun87 <sarun87@gmail.com>
2011-10-25 17:09:14 +0000

    Added functionality required by the receiver. TODO: Probabilistically drop segments. Add checksum code.

---
+++ p2mpserver/src/p2mp/InternetChecksum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2mp;
+
+/**
+ *
+ * @author Shyam
+ */
+public class InternetChecksum {
+
+    public static long getCheckSum(byte[] bs, byte[] checksum) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
