50b1d5d4b634a70ec67a45ec7bb178929c6db0b0
sarun87@gmail.com <sarun87@gmail.com@94d018cb-d97d-bea0-d574-3dc08b68d47e>
2011-10-26 03:37:19 +0000

    Removed files - Interfaces first defined, now not being used.

--- trunk/p2mpclient/src/p2mp/PacketListner.java
+++
@@ -1,9 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package p2mp;
-
-public interface PacketListner {
-    public void PacketReceived(PacketEvent pktEvent);
-}
