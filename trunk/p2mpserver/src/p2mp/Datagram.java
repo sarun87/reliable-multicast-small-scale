dfe9738c2609adaa4636971e6809c675ddf66953
sarun87@gmail.com <sarun87@gmail.com@94d018cb-d97d-bea0-d574-3dc08b68d47e>
2011-10-31 17:32:47 +0000

    Added comments and license. Code reformatting.

--- trunk/p2mpserver/src/p2mp/Datagram.java
+++ trunk/p2mpserver/src/p2mp/Datagram.java
@@ -1,39 +1,97 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/** ********************************************************************
+ * File:           Datagram.java 
+ * Description:    Consists the datagram object.
+ * Authors:        Arun, Shyam, Rahul, Venkatesh 
+ * Created:        Sun Oct 23 01:17:04 EST 2011
+ *
+ * (C) Copyright 2011
+ ** Licensed under the GPL License, Version 3.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ ** http://www.gnu.org/licenses/gpl-3.0.txt
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ *
+ **********************************************************************/
+
 package p2mp;
 
 import java.nio.ByteBuffer;
 
+/**
+ * Create a datagram that contains header and body of the datagarm.
+ */
 public class Datagram {
 
-    public byte[] sequenceNumber;
-    public byte[] checksum;
-    public byte[] datagramType;
-    public byte[] data;
-    public byte[] dataSize;
+	/**
+	 * Holds the sequencee number of the datagram.
+	 */
+	public byte[] sequenceNumber;
 
-    public Datagram(byte[] data, int dataSize, boolean isDataPacket) {
+	/**
+	 * Holds the checksum for the body. Checksum not implemented as sudo-header
+	 */
+	public byte[] checksum;
 
-    }
-    public Datagram(){
-    	data = null;
-    	dataSize = ByteBuffer.allocate(4).putInt(0,0).array();
-    	datagramType = null;
-    	checksum = ByteBuffer.allocate(2).putChar((char)0).array();
-    	sequenceNumber = ByteBuffer.allocate(4).putInt(0,0).array();
-    	
-    	
-    }
-    public byte[] getBytes() {
-        byte[] temp = new byte[sequenceNumber.length + checksum.length + datagramType.length + dataSize.length];
-        System.arraycopy(sequenceNumber, 0, temp, 0, sequenceNumber.length);
-        System.arraycopy(checksum, 0, temp, sequenceNumber.length, checksum.length);
-        System.arraycopy(datagramType, 0, temp, sequenceNumber.length + checksum.length, datagramType.length);
-        System.arraycopy(dataSize, 0, temp, sequenceNumber.length + checksum.length + datagramType.length, dataSize.length);
-        //System.arraycopy(data, 0, temp, sequenceNumber.length + checksum.length + datagramType.length + dataSize.length, data.length);
-        
-        return temp;
-    }
+	/**
+	 * Holds datagramType which is either DATA or ACKNOWLEDGMENT
+	 */
+	public byte[] datagramType;
+
+	/**
+	 * Holds the data in bytes.
+	 */
+	public byte[] data;
+
+	/**
+	 * Holds the size of data that the object "data" cotains.
+	 */
+	public byte[] dataSize;
+
+	/**
+	 * Constructor to create a datagram object
+	 * 
+	 * @param data
+	 *            Data to be stored by the datagram
+	 * @param dataSize
+	 *            Datasize of the data
+	 * @param isDataPacket
+	 *            packet is data or ack
+	 */
+	public Datagram(byte[] data, int dataSize, boolean isDataPacket) {
+
+	}
+
+	/**
+	 * Default constructor that initializes the datagram object to null/zero
+	 */
+	public Datagram() {
+		data = null;
+		dataSize = ByteBuffer.allocate(4).putInt(0, 0).array();
+		datagramType = null;
+		checksum = ByteBuffer.allocate(2).putChar((char) 0).array();
+		sequenceNumber = ByteBuffer.allocate(4).putInt(0, 0).array();
+
+	}
+
+	/**
+	 * Get the datagram as a byte array. Used to send it over the socket.
+	 * 
+	 * @return entire packet in bytes
+	 */
+	public byte[] getBytes() {
+		byte[] temp = new byte[sequenceNumber.length + checksum.length
+				+ datagramType.length + dataSize.length];
+		System.arraycopy(sequenceNumber, 0, temp, 0, sequenceNumber.length);
+		System.arraycopy(checksum, 0, temp, sequenceNumber.length,
+				checksum.length);
+		System.arraycopy(datagramType, 0, temp, sequenceNumber.length
+				+ checksum.length, datagramType.length);
+		System.arraycopy(dataSize, 0, temp, sequenceNumber.length
+				+ checksum.length + datagramType.length, dataSize.length);
+		return temp;
+	}
 }
--- trunk/p2mpserver/src/p2mp/p2mpserver.java
+++ trunk/p2mpserver/src/p2mp/p2mpserver.java
@@ -1,3 +1,22 @@
+/** ********************************************************************
+ * File:           p2mpserver.java 
+ * Description:    The server code running on multicast node that recei-
+ * 				   -ves the file.
+ * Authors:        Arun, Shyam, Rahul, Venkatesh 
+ * Created:        Thu Oct 20 12:01:05 EST 2011
+ *
+ * (C) Copyright 2011
+ ** Licensed under the GPL License, Version 3.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ ** http://www.gnu.org/licenses/gpl-3.0.txt
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ *
+ **********************************************************************/
 package p2mp;
 
 import java.io.File;
@@ -5,14 +24,20 @@ import java.io.FileOutputStream;
 import java.net.*;
 import java.nio.ByteBuffer;
 
-
 /**
- * 
- * @author arun
- * 
+ * Class contains the main method that runs the program.
  */
 class p2mpserver {
+
+	/**
+	 * Main method - start p2mpserver and wait for the multicast.
+	 * 
+	 * @param args
+	 *            - portNo fileName WindowSize LossProbability
+	 * @throws Exception
+	 */
 	public static void main(String args[]) throws Exception {
+
 		// Initialize the parameters given using the command-line
 		Integer portNo = Integer.parseInt(args[0]);
 		String fileName = args[1];
@@ -20,150 +45,198 @@ class p2mpserver {
 		double packetLossProbability = Double.parseDouble(args[3]);
 
 		// Set the dataRepository
-		DataRepository.setWindowSize(windowSize);
-		DataRepository.setPortNumber(portNo);
+		DataRepository.WINDOWSIZE = windowSize;
+		DataRepository.SENDER_PORT_NUMBER = portNo;
 		DataRepository.fileName = fileName;
 
 		// Open file that has to be written into
-		//FileWriter fwriter = new FileWriter(fileName);
-		//BufferedWriter out = new BufferedWriter(fwriter);
 		File file = new File(fileName);
 		FileOutputStream out = new FileOutputStream(file);
-		
+
 		// Start waiting for packets
 		DatagramSocket serverSocket = new DatagramSocket(portNo);
 		System.out.println("Init Complete. Waiting for incoming packets...");
 		byte[] receiveData = new byte[1036];
 		byte[] sendData = new byte[12];
+		// Continue till end of file has been received.
 		while (true) {
 			DatagramPacket receivePacket = new DatagramPacket(receiveData,
 					receiveData.length);
 			serverSocket.receive(receivePacket);
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
-			
-			//System.out.println("Segment received! Extracting packet Information now..");
-			// 12 byte header extraction
 
-			int seqNumber = ByteBuffer.allocate(4).put(receiveData, 0, 4).getInt(0);
-			
-			// CHECK WITH PACKET LOSS PROBABILITY TO SIMULATE LOST PACKETS //////
+			// @Debug
+			// System.out.println("Segment received! Extracting packet Information now..");
+
+			// 12 byte header extraction
+			int seqNumber = ByteBuffer.allocate(4).put(receiveData, 0, 4)
+					.getInt(0);
+
+			// CHECK WITH PACKET LOSS PROBABILITY TO SIMULATE LOST PACKETS
+			// //////
 			double randomProb = Math.random();
-			if(randomProb < packetLossProbability){
+			if (randomProb < packetLossProbability) {
 				// Discard packet. Do nothing.
-				System.out.println("Packet Loss, sequence Number = "+ seqNumber );
+				System.out.println("Packet Loss, sequence Number = "
+						+ seqNumber);
 				continue;
 			}
-			//////////////////////////
-			
+			// ////////////////////////
+
+			// Build the datagram from the bytes received.
 			Datagram receivedDatagram = new Datagram();
-			receivedDatagram.sequenceNumber = ByteBuffer.allocate(4).putInt(0,seqNumber).array();
-			receivedDatagram.checksum = ByteBuffer.allocate(2).put(receiveData,4,2).array();
-			receivedDatagram.datagramType = ByteBuffer.allocate(2).put(receiveData, 6, 2).array();
-			receivedDatagram.dataSize = ByteBuffer.allocate(4).put(receiveData,8,4).array();
-			int dataSize = ByteBuffer.allocate(4).put(receivedDatagram.dataSize).getInt(0);
+			receivedDatagram.sequenceNumber = ByteBuffer.allocate(4)
+					.putInt(0, seqNumber).array();
+			receivedDatagram.checksum = ByteBuffer.allocate(2)
+					.put(receiveData, 4, 2).array();
+			receivedDatagram.datagramType = ByteBuffer.allocate(2)
+					.put(receiveData, 6, 2).array();
+			receivedDatagram.dataSize = ByteBuffer.allocate(4)
+					.put(receiveData, 8, 4).array();
+			int dataSize = ByteBuffer.allocate(4)
+					.put(receivedDatagram.dataSize).getInt(0);
 			// Data Extraction by using the dataSize present in the header.
-			receivedDatagram.data = ByteBuffer.allocate(dataSize).put(receiveData,12,dataSize).array();
+			receivedDatagram.data = ByteBuffer.allocate(dataSize)
+					.put(receiveData, 12, dataSize).array();
 			// Check checksum
-			long checkSumResult = InternetChecksum.getCheckSum(ByteBuffer.allocate(dataSize).put(receiveData,12,dataSize).array());
-			long checkSum = (long)ByteBuffer.allocate(2).put(receivedDatagram.checksum).getChar(0);
-			if(checkSumResult != checkSum ){
+			long checkSumResult = InternetChecksum.getCheckSum(ByteBuffer
+					.allocate(dataSize).put(receiveData, 12, dataSize).array());
+			long checkSum = (long) ByteBuffer.allocate(2)
+					.put(receivedDatagram.checksum).getChar(0);
+			// Check if the datagram is intact. Note: pseudo-header checksum not
+			// implemented.
+			if (checkSumResult != checkSum) {
 				// Discard packet, do nothing
-				//System.out.println("Error in packet with seq:"+seqNumber);
+				// System.out.println("Error in packet with seq:"+seqNumber);
 				continue;
 			}
-			if(seqNumber == DataRepository.expectedSequenceNumber){
+			// Check the sequence number of the datagram received
+			if (seqNumber == DataRepository.expectedSequenceNumber) {
 				// Expected in-sequence segment has arrived
-				// send this and the other in-sequence packets to the upper layer.
-				//System.out.println("In sequence packet with seq:"+seqNumber);
+				// send this and the other in-sequence packets to the upper
+				// layer.
+				// @Debug
+				// System.out.println("In sequence packet with seq:"+seqNumber);
 				SlidingWindow.addItemToWindow(seqNumber, receivedDatagram);
-				
-				if(ByteBuffer.allocate(2).put(receivedDatagram.datagramType).getChar(0) == DataRepository.ACKPACKET){
-					System.out.println("END OF FILE RECEIVED! File Transfer complete!");
+
+				// Check to see if the END of File is notified. In this case,
+				// it's an ACK packet from the sender.
+				if (ByteBuffer.allocate(2).put(receivedDatagram.datagramType)
+						.getChar(0) == DataRepository.ACKPACKET) {
+					System.out
+							.println("END OF FILE RECEIVED! File Transfer complete!");
 					break;
 				}
-				
+
+				// Get the maximum segment that is in-order in the window.
 				int maxSeqInOrder;
-				for(maxSeqInOrder = seqNumber; maxSeqInOrder < SlidingWindow.StartingSeqNumber + DataRepository.WINDOWSIZE; ++maxSeqInOrder){
+				for (maxSeqInOrder = seqNumber; maxSeqInOrder < SlidingWindow.StartingSeqNumber
+						+ DataRepository.WINDOWSIZE; ++maxSeqInOrder) {
 					Datagram value = SlidingWindow.Window.get(maxSeqInOrder);
-					if (value != null){
-						DataRepository.expectedSequenceNumber = maxSeqInOrder+1;
-					}
-					else{
+					if (value != null) {
+						DataRepository.expectedSequenceNumber = maxSeqInOrder + 1;
+					} else {
 						break;
 					}
 				}
 				maxSeqInOrder = maxSeqInOrder - 1;
-				//System.out.print("Sending segments to upper layer:");
-				for(int count = SlidingWindow.StartingSeqNumber; count <= maxSeqInOrder; ++count){
+				// @Debug System.out.print("Sending segments to upper layer:");
+				// Send the in-order packets to the application (upper layer)
+				for (int count = SlidingWindow.StartingSeqNumber; count <= maxSeqInOrder; ++count) {
 					Datagram dgToBeWritten = SlidingWindow.Window.get(count);
 					out.write(dgToBeWritten.data);
 					SlidingWindow.removeItemFromWindow(count);
-					//System.out.print(" "+count+" ");
+					// @Debug System.out.print(" "+count+" ");
 				}
-				//System.out.println("");
+				// @Debug System.out.println("");
 
-				// Construct Ack packet
+				// Construct Acknowledgment packet for the received sequence
+				// number
 				Datagram acknowledgmentPacket = new Datagram();
-				acknowledgmentPacket.datagramType = ByteBuffer.allocate(2).putChar((char)DataRepository.ACKPACKET).array();
-				acknowledgmentPacket.sequenceNumber= ByteBuffer.allocate(4).putInt(0,DataRepository.expectedSequenceNumber-1).array();
+				acknowledgmentPacket.datagramType = ByteBuffer.allocate(2)
+						.putChar((char) DataRepository.ACKPACKET).array();
+				acknowledgmentPacket.sequenceNumber = ByteBuffer.allocate(4)
+						.putInt(0, DataRepository.expectedSequenceNumber - 1)
+						.array();
 				sendData = acknowledgmentPacket.getBytes();
-				
+
 				// Construct a java datagram packet and send it.
 				DatagramPacket sendPacket = new DatagramPacket(sendData,
-									sendData.length, IPAddress, port);
+						sendData.length, IPAddress, port);
 				serverSocket.send(sendPacket);
-				//System.out.println("Ack:"+(DataRepository.expectedSequenceNumber -1)+ " sent");
-				
-			}
-			else
-			{
-				if(seqNumber > DataRepository.expectedSequenceNumber && seqNumber < SlidingWindow.StartingSeqNumber + DataRepository.WINDOWSIZE){
-					// Out of sequence packet. Buffer it and send ack for expected Sequence Number - 1 (Previously ack'ed packet)
-					//System.out.println("Out-of-Seq segment recieved with seq:"+seqNumber);
+				// @Debug
+				// System.out.println("Ack:"+(DataRepository.expectedSequenceNumber
+				// -1)+ " sent");
+
+			} else {
+				// Not the in-order sequence number. Check for datagram already
+				// ack-ed, out-of-sequence or invalid packet
+				if (seqNumber > DataRepository.expectedSequenceNumber
+						&& seqNumber < SlidingWindow.StartingSeqNumber
+								+ DataRepository.WINDOWSIZE) {
+					// Out of sequence packet. Buffer it and send ack for
+					// expected Sequence Number - 1 (Previously ack'ed packet)
+					// @Debug
+					// System.out.println("Out-of-Seq segment recieved with seq:"+seqNumber);
 					SlidingWindow.addItemToWindow(seqNumber, receivedDatagram);
-					
+
 					// Send ack with packet previously ack'ed.
 					Datagram acknowledgmentPacket = new Datagram();
-					acknowledgmentPacket.datagramType = ByteBuffer.allocate(2).putChar((char)DataRepository.ACKPACKET).array();
-					acknowledgmentPacket.sequenceNumber= ByteBuffer.allocate(4).putInt(0,DataRepository.expectedSequenceNumber-1).array();
+					acknowledgmentPacket.datagramType = ByteBuffer.allocate(2)
+							.putChar((char) DataRepository.ACKPACKET).array();
+					acknowledgmentPacket.sequenceNumber = ByteBuffer
+							.allocate(4)
+							.putInt(0,
+									DataRepository.expectedSequenceNumber - 1)
+							.array();
 					sendData = acknowledgmentPacket.getBytes();
-					
-					//System.out.println("Ack:"+(DataRepository.expectedSequenceNumber-1)+" sent");
+
+					// @Debug
+					// System.out.println("Ack:"+(DataRepository.expectedSequenceNumber-1)+" sent");
 					// Construct a java datagram packet and send it.
 					DatagramPacket sendPacket = new DatagramPacket(sendData,
-										sendData.length, IPAddress, port);
+							sendData.length, IPAddress, port);
 					serverSocket.send(sendPacket);
-				}
-				else if(seqNumber < DataRepository.expectedSequenceNumber){
-					// Already acke'd. Re-ack with the last packet that has already been sent to the higher layer.
-					//System.out.println("Already ack-ed segment received with seq:"+seqNumber);
-					
+				} else if (seqNumber < DataRepository.expectedSequenceNumber) {
+					// Already acke'd. Re-ack with the last packet that has
+					// already been sent to the higher layer.
+					// @Debug
+					// System.out.println("Already ack-ed segment received with seq:"+seqNumber);
+
 					// Send ack with packet previously ack'ed.
 					Datagram acknowledgmentPacket = new Datagram();
-					acknowledgmentPacket.datagramType = ByteBuffer.allocate(2).putChar((char)DataRepository.ACKPACKET).array();
-					acknowledgmentPacket.sequenceNumber= ByteBuffer.allocate(4).putInt(0,DataRepository.expectedSequenceNumber-1).array();
+					acknowledgmentPacket.datagramType = ByteBuffer.allocate(2)
+							.putChar((char) DataRepository.ACKPACKET).array();
+					acknowledgmentPacket.sequenceNumber = ByteBuffer
+							.allocate(4)
+							.putInt(0,
+									DataRepository.expectedSequenceNumber - 1)
+							.array();
 					sendData = acknowledgmentPacket.getBytes();
-					
-					//System.out.println("Ack:"+(DataRepository.expectedSequenceNumber - 1)+ " sent");
+
+					// @Debug
+					// System.out.println("Ack:"+(DataRepository.expectedSequenceNumber
+					// - 1)+ " sent");
 					// Construct a java datagram packet and send it.
 					DatagramPacket sendPacket = new DatagramPacket(sendData,
-										sendData.length, IPAddress, port);
+							sendData.length, IPAddress, port);
 					serverSocket.send(sendPacket);
-				}
-				else if(ByteBuffer.allocate(2).put(receivedDatagram.datagramType).getChar(0) == DataRepository.ACKPACKET){
-					System.out.println("END OF FILE RECEIVED! File Transfer complete!");
+				} else if (ByteBuffer.allocate(2)
+						.put(receivedDatagram.datagramType).getChar(0) == DataRepository.ACKPACKET) {
+					System.out
+							.println("END OF FILE RECEIVED! File Transfer complete!");
 					break;
-				}
-				else
-				{
+				} else {
 					// Packet out of sequence and invalid.
 					// Drop packet and do nothing.
-					//System.out.println("Invalid packet/packet not in window. seq:"+seqNumber);
+					// @Debug
+					// System.out.println("Invalid packet/packet not in window. seq:"+seqNumber);
 					continue;
 				}
-			}  // End of else - Not the right seq number
+			} // End of else - Not the right seq number
 		} // End of while construct
+			// Close the file
 		out.close();
 	} // End of Method - main
 } // End of Method - class
